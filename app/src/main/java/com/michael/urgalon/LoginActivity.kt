5fbd0a19eb93537efd72c77366cc8d493928adc1
MichaelMathew <2072007@maranatha.ac.id>
2022-11-18 22:53:07 +0700

    add depedencies , network, values dan activity login
    add depedencies , network, values dan activity login

---
+++ app/src/main/java/com/michael/urgalon/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.michael.urgalon
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.michael.urgalon.databinding.ActivityLoginBinding
+
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
