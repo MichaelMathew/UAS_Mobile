a638af95300b5957184659ccffec3cb0f3796c4b
MichaelMathew <2072007@maranatha.ac.id>
2022-11-19 00:03:49 +0700

    menambahkan home activity dan melakukan perubahan pada activity lainnya
    menambahkan home activity dan melakukan perubahan pada activity lainnya

--- app/src/main/java/com/michael/urgalon/LoginActivity.kt
+++ app/src/main/java/com/michael/urgalon/LoginActivity.kt
@@ -1,7 +1,9 @@
 package com.michael.urgalon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
 import com.michael.urgalon.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
@@ -10,5 +12,29 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        binding.tvSignup.setOnClickListener{
+            val intent = Intent(this@LoginActivity,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnLogin.setOnClickListener {
+
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            if(email == "michael@gmail.com" && password == "12345"){
+                val intent = Intent(this@LoginActivity, HomeActivity::class.java)
+                startActivity(intent)
+                this.finish()
+            } else {
+                if(email.trim().isEmpty()){
+                    binding.etEmail.error = resources.getString(R.string.message_error_email_empty)
+                }
+                if(password.trim().isEmpty()){
+                    binding.etPassword.error = resources.getString(R.string.message_error_pw_empty)
+                }
+                Snackbar.make(this,binding.btnLogin,resources.getString(R.string.msg_failed_login)
+                    , Snackbar.LENGTH_LONG).show()
+            }
+        }
     }
 }
--- app/src/main/java/com/michael/urgalon/SignUpActivity.kt
+++ app/src/main/java/com/michael/urgalon/SignUpActivity.kt
@@ -1,11 +1,50 @@
 package com.michael.urgalon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
+import com.michael.urgalon.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
+        binding = ActivitySignUpBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.tvLogin.setOnClickListener {
+            val intent = Intent(this@SignUpActivity,LoginActivity::class.java)
+            startActivity(intent)
+        }
+        binding.btnSignUp.setOnClickListener {
+            val username = binding.etUsername.text.toString()
+            val email = binding.etEmail.text.toString()
+            val password = binding.etPassword.text.toString()
+            val confirmpassword = binding.etConfirmpassword.text.toString()
+            if (username.trim().isNotEmpty() && email.trim().isNotEmpty() && password.trim().isNotEmpty() && confirmpassword.trim().isNotEmpty()){
+                if (password.trim() != confirmpassword.trim()) {
+                    Toast.makeText(this@SignUpActivity, "Password dan Confirm Password tidak sama", Toast.LENGTH_SHORT).show()
+                }
+                else {
+                    val intent = Intent(this@SignUpActivity, LoginActivity::class.java)
+                    startActivity(intent)
+                }
+            }
+            if(username.trim().isEmpty()){
+                binding.etUsername.error = resources.getString(R.string.message_error_username_empty)
+            }
+            if(email.trim().isEmpty()){
+                binding.etEmail.error = resources.getString(R.string.message_error_email_empty)
+            }
+            if(password.trim().isEmpty()){
+                binding.etPassword.error = resources.getString(R.string.message_error_pw_empty)
+            }
+            if(confirmpassword.trim().isEmpty()){
+                binding.etConfirmpassword.error = resources.getString(R.string.message_error_confirmpw_empty)
+            }
+            Snackbar.make(this,binding.btnSignUp,resources.getString(R.string.msg_failed_signUp)
+                , Snackbar.LENGTH_LONG).show()
+        }
     }
 }
