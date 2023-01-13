e9adfbf867812f1d2eb2b2a0c92d62fd53258cd4
MichaelMathew <2072007@maranatha.ac.id>
2023-01-14 00:51:10 +0700

    login dengan google
    login dengan google

--- app/src/main/java/com/michael/urgalon/LoginActivity.kt
+++ app/src/main/java/com/michael/urgalon/LoginActivity.kt
@@ -3,10 +3,16 @@ package com.michael.urgalon
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
@@ -21,6 +27,7 @@ import com.michael.urgalon.fragment.ProfileFragment
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
+    private lateinit var googleSignInClient: GoogleSignInClient
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -70,6 +77,17 @@ class LoginActivity : AppCompatActivity() {
                 }
             }
         }
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        googleSignInClient = GoogleSignIn.getClient(this, gso)
+        binding.logingoogle.setOnClickListener {
+            startActivityForResult(
+                googleSignInClient.signInIntent,
+                9001
+            )
+        }
     }
 
     override fun onStart() {
@@ -84,6 +102,44 @@ class LoginActivity : AppCompatActivity() {
         val intent = Intent(this@LoginActivity, HomeActivity::class.java);
         startActivity(intent);
         this.finish();
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        firebaseAuth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    if (checkUserData(task.result.user!!.uid) != null) {
+                        updateUI()
+                    } else {
+                        saveData(User(task.result.user!!.uid, task.result.user?.email, 0))?.addOnSuccessListener {
+                            updateUI()
+                        }
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Toast.makeText(this@LoginActivity, task.exception.toString(), Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
+        if (requestCode == 9001) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("TAG", "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("TAG", "Google sign in failed", e)
+            }
+        }
     }
 
     private fun checkUserData(id: String): User? {
