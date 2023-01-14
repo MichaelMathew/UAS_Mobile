38787a625da4eadc575a1dca0f198952548b90a8
Arya Majiah <71741042+aryatripm@users.noreply.github.com>
2023-01-14 10:11:45 +0700

    fix google login

--- app/src/main/java/com/michael/urgalon/LoginActivity.kt
+++ app/src/main/java/com/michael/urgalon/LoginActivity.kt
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.auth.api.identity.BeginSignInRequest
+import com.google.android.gms.auth.api.identity.Identity
+import com.google.android.gms.auth.api.identity.SignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
@@ -28,6 +31,11 @@ class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
     private lateinit var firebaseAuth: FirebaseAuth
     private lateinit var googleSignInClient: GoogleSignInClient
+    private val REQ_ONE_TAP = 2
+    private var showOneTapUI = true
+    private lateinit var auth: FirebaseAuth
+    private lateinit var oneTapClient: SignInClient
+    private lateinit var signInRequest: BeginSignInRequest
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -82,12 +90,23 @@ class LoginActivity : AppCompatActivity() {
             .requestEmail()
             .build()
         googleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
         binding.logingoogle.setOnClickListener {
             startActivityForResult(
                 googleSignInClient.signInIntent,
                 9001
             )
         }
+
+        oneTapClient = Identity.getSignInClient(this)
+        signInRequest = BeginSignInRequest.builder()
+            .setGoogleIdTokenRequestOptions(
+                BeginSignInRequest.GoogleIdTokenRequestOptions.builder()
+                    .setSupported(true)
+                    .setServerClientId(getString(R.string.default_web_client_id))
+                    .setFilterByAuthorizedAccounts(true)
+                    .build())
+            .build()
     }
 
     override fun onStart() {
@@ -117,7 +136,6 @@ class LoginActivity : AppCompatActivity() {
                         }
                     }
                 } else {
-                    // If sign in fails, display a message to the user.
                     Toast.makeText(this@LoginActivity, task.exception.toString(), Toast.LENGTH_SHORT)
                         .show()
                 }
@@ -127,27 +145,66 @@ class LoginActivity : AppCompatActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        // Result returned from launching the Intent from GoogleSignInApi.getSignInIntent(...);
         if (requestCode == 9001) {
             val task = GoogleSignIn.getSignedInAccountFromIntent(data)
             try {
-                // Google Sign In was successful, authenticate with Firebase
                 val account = task.getResult(ApiException::class.java)!!
                 Log.d("TAG", "firebaseAuthWithGoogle:" + account.id)
                 firebaseAuthWithGoogle(account.idToken!!)
             } catch (e: ApiException) {
-                // Google Sign In failed, update UI appropriately
                 Log.w("TAG", "Google sign in failed", e)
             }
         }
     }
 
-    private fun checkUserData(id: String): User? {
-        var user: User? = null
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//
+//        when (requestCode) {
+//            REQ_ONE_TAP -> {
+//                try {
+//                    val credential = oneTapClient.getSignInCredentialFromIntent(data)
+//                    val idToken = credential.googleIdToken
+//                    when {
+//                        idToken != null -> {
+//                            val firebaseCredential = GoogleAuthProvider.getCredential(idToken, null)
+//                            auth.signInWithCredential(firebaseCredential)
+//                                .addOnCompleteListener(this) { task ->
+//                                    if (task.isSuccessful) {
+//                                        Log.d("ARYA", "signInWithCredential:success")
+////                                        val user = auth.currentUser
+////                                        updateUI()
+//                                        if (checkUserData(task.result.user!!.uid) != null) {
+//                                            updateUI()
+//                                        } else {
+//                                            saveData(User(task.result.user!!.uid, task.result.user?.email, 0))?.addOnSuccessListener {
+//                                                updateUI()
+//                                            }
+//                                        }
+//                                    } else {
+//                                        Log.w("ARYA", "signInWithCredential:failure", task.exception)
+////                                        updateUI()
+//                                    }
+//                                }
+//                        }
+//                        else -> {
+//                            // Shouldn't happen.
+//                            Log.d("ARYA", "No ID token!")
+//                        }
+//                    }
+//                } catch (e: ApiException) {
+//                    Log.d("CATCH", e.message.toString())
+//                }
+//            }
+//        }
+//    }
+
+    private fun checkUserData(id: String): Any? {
+        var user: Any? = null
         val database = Firebase.database("https://urgalon-125ed-default-rtdb.asia-southeast1.firebasedatabase.app/")
         val reference = database.getReference(ProfileFragment.USER_REF)
         reference.child(id).get().addOnSuccessListener {
-            user = it.value as User
+            user = it.value
         }
         return user
     }
